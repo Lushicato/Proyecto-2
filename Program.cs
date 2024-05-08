ca4b94a36c17eb41b53dda7b6a4be838de6e5ec8
Lushicato <franciscojgt44@gmail.com>
2024-05-08 17:20:59 -0600

    Se hizo el menu por tipo de vehiculo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Estacionamiento
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido al estacionamiento UNICAES");
+        Console.WriteLine("Por favor elija una opcion de su tipo de vehiculo");
+        bool salir=false;
+        while(!salir)
+        {
+            Console.WriteLine("1. Motocicleta");
+            Console.WriteLine("2. Sedan");
+            Console.WriteLine("3. Camioneta");
+            Console.WriteLine("4. Microbus");
+            Console.WriteLine("5. Salir");
+            int Opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (Opcion)
+            {
+
+            }
+        }
+    }
+}
