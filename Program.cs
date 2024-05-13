35da06027d4ca3e0f951b488184072c56cb4620c
Rony Herrera <ronyherreramaldonado@gmail.com>
2024-05-13 15:39:01 -0600

    Ya que la grafica que hice estaba mal la borre XD

--- Program.cs
+++ Program.cs
@@ -1,67 +1,23 @@
-﻿class Estacionamiento
+﻿static void Main(string[] args)
 {
-    public class grafica
+    Console.WriteLine("Bienvenido al estacionamiento UNICAES");
+    Console.WriteLine("Por favor elija una opcion de su tipo de vehiculo");
+    bool salir = false;
+
+    while (!salir)
     {
-        bool[] estacionamientos = new bool[30]; 
-        public void dibujar()
-        {
-          
-            Console.WriteLine("Estacioamientos dispobibles");
+        Console.WriteLine("1. Motocicleta");
+        Console.WriteLine("2. Sedan");
+        Console.WriteLine("3. Camioneta");
+        Console.WriteLine("4. Microbus");
+        Console.WriteLine("5. Salir");
 
-            for (int i = 0; i < estacionamientos.Length; i++)
-            {
-                Console.Write("estacionamiento " + (i + 1) + ": ");
-                if (estacionamientos[i])
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                }
-                Console.WriteLine(Rectangulo(5, 3)); 
-                Console.ResetColor(); 
-            }
-        }
+        int opcion = Convert.ToInt32(Console.ReadLine());
 
-        private string Rectangulo(int altura, int ancho)
+        switch (opcion)
         {
-            string rectangulo = "";
-            for (int i = 0; i < altura; i++)
-            {
-                for (int j = 0; j < ancho ; j++)
-                {
-                    rectangulo += "*";
-                }
-                rectangulo += "\n"; 
-            }
-           
+            case 1:
+               
         }
     }
-
-}
-
-static void Main(string[] args
-    {
-       
-        Console.WriteLine("Bienvenido al estacionamiento UNICAES");
-        Console.WriteLine("Por favor elija una opcion de su tipo de vehiculo");
-        bool salir=false;
-        while(!salir)
-        {
-            Console.WriteLine("1. Motocicleta");
-            Console.WriteLine("2. Sedan");
-            Console.WriteLine("3. Camioneta");
-            Console.WriteLine("4. Microbus");
-            Console.WriteLine("5. Salir");
-            int Opcion = Convert.ToInt32(Console.ReadLine());
-
-            switch (Opcion)
-            {
-
-            }
-        }
-    }
-
-   
 }
